950ecdc2af37e8b293dd1b3741e00ef33a83ae69
King Elisha <ek.chibueze@gmail.com>
2020-03-16 17:33:06 +0100

    updated mail client

--- routes/skyblazar/email.js
+++ routes/skyblazar/email.js
@@ -23,9 +23,10 @@ const sendProjectEmail = async details => {
 
   // create reusable transporter object using the default SMTP transport
   let transporter = nodemailer.createTransport({
-    host: "skyblazar.com",
-    port: 465,
-    secure: true, // true for 465, false for other ports
+    // host: "skyblazar.com",
+    // port: 465,
+    // secure: true, // true for 465, false for other ports
+    service: "gmail",
     auth: {
       user: emailAccount.user,
       pass: emailAccount.password
