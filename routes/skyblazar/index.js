d4ccea1ef0be6360f9fc25bcaabe2289a7d491bd
King Elisha <ek.chibueze@gmail.com>
2020-03-16 16:26:50 +0100

    update routes

--- routes/skyblazar/index.js
+++ routes/skyblazar/index.js
@@ -1,26 +1,29 @@
-const router = require('express').Router();
-const path = require('path');
-const { sendProjectEmail } = require('./email');
+const router = require("express").Router();
+const path = require("path");
+const { sendProjectEmail } = require("./email");
 
-router.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
+// router.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "index.html"));
+// });
 
-router.get("/articles", (req, res) => {
-  res.sendFile(path.join(__dirname, "articles.html"));
-});
+// router.get("/articles", (req, res) => {
+//   res.sendFile(path.join(__dirname, "articles.html"));
+// });
 
-router.get("/articles/:article", (req, res) => {
-  res.sendFile(path.join(__dirname, "articles", `${req.params.article}.html`));
-});
+// router.get("/articles/:article", (req, res) => {
+//   res.sendFile(path.join(__dirname, "articles", `${req.params.article}.html`));
+// });
 
 router.post("/project", (req, res) => {
-  sendProjectEmail(req.body)
-    .then((sent) => {
-      if (sent) return res.json({ success: true });
+  sendProjectEmail(req.body).then(sent => {
+    if (sent) return res.json({ success: true });
 
-      res.json({ success: false });
-    });
+    res.json({ success: false });
+  });
+});
+
+router.get("*", (_, res) => {
+  res.status(404).send("404: Not Found");
 });
 
 module.exports = router;
